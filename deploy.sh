5a76776473c2aad1e63a97a5c68932bf516da15d
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-10-11 19:06:47 +0200

    fix(deploy): less envs

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@ set -e
 
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm  \
-    -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP}`
+    -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD  \
+    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola processor-add-filename-column`
 docker tag keboola/processor-add-filename-column:latest ${REPOSITORY}:${TRAVIS_TAG}
 docker tag keboola/processor-add-filename-column:latest ${REPOSITORY}:latest
 eval $(docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP})
+    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola processor-add-filename-column)
 docker push ${REPOSITORY}:${TRAVIS_TAG}
 docker push ${REPOSITORY}:latest
 
 # Deploy to KBC -> update the tag in Keboola Developer Portal (needs $KBC_DEVELOPERPORTAL_VENDOR & $KBC_DEVELOPERPORTAL_APP)
 docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest update-app-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} ${TRAVIS_TAG}
+    quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola processor-add-filename-column ${TRAVIS_TAG}
